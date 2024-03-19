b81da692ed4de6f63069543ae1d036cda2612d35
ccgauche <gaucheron.laurent@gmail.com>
2024-03-19 17:41:09 +0100

    Refactor cipher

--- src/parser/cipher.rs
+++ src/parser/cipher.rs
@@ -1,25 +1,35 @@
 use boa_engine::Context;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct DecipherQuery {
+    url: String,
+    s: String,
+    sp: Option<String>,
+}
+
+#[cfg_attr(feature = "performance_analysis", flamer::flame)]
+fn decode_url(url: &str) -> Option<DecipherQuery> {
+    serde_qs::from_str(url).ok()
+}
 
 #[cfg_attr(feature = "performance_analysis", flamer::flame)]
 pub fn decipher(
-    url: &str,
+    base_url: &str,
     decipher_script_string: &(String, String),
     cipher_cache: &mut Option<(String, Context)>,
 ) -> String {
-    let args: serde_json::value::Map<String, serde_json::Value> = {
-        #[cfg(feature = "performance_analysis")]
-        let _guard = flame::start_guard("serde_qs::from_str");
-        serde_qs::from_str(url).unwrap()
+    if decipher_script_string.1.is_empty() {
+        return base_url.to_string();
+    }
+    let args: DecipherQuery = if let Some(e) = decode_url(base_url) {
+        e
+    } else {
+        return base_url.to_string();
     };
 
-    if args.get("s").is_none() || decipher_script_string.1.is_empty() {
-        if args.get("url").is_none() {
-            return url.to_string();
-        } else {
-            let args_url = args.get("url").and_then(|x| x.as_str()).unwrap_or("");
-            return args_url.to_string();
-        }
-    }
+    let url = args.url;
+    let s = args.s;
 
     let context = match cipher_cache {
         Some((ref cache_key, ref mut context)) if cache_key == &decipher_script_string.1 => context,
@@ -31,12 +41,7 @@ pub fn decipher(
                 decipher_script_string.1.as_str(),
             ));
             if decipher_script.is_err() {
-                if args.get("url").is_none() {
-                    return url.to_string();
-                } else {
-                    let args_url = args.get("url").and_then(|x| x.as_str()).unwrap_or("");
-                    return args_url.to_string();
-                }
+                return url;
             }
             *cipher_cache = Some((decipher_script_string.1.to_string(), context));
             &mut cipher_cache.as_mut().unwrap().1
@@ -46,72 +51,30 @@ pub fn decipher(
     let result = {
         #[cfg(feature = "performance_analysis")]
         let _guard = flame::start_guard("execute engine");
-        context.eval(boa_engine::Source::from_bytes(&format!(
-            r#"{func_name}("{args}")"#,
-            func_name = decipher_script_string.0.as_str(),
-            args = args.get("s").and_then(|x| x.as_str()).unwrap_or("")
-        )))
+        context
+            .eval(boa_engine::Source::from_bytes(&format!(
+                r#"{func_name}("{args}")"#,
+                func_name = decipher_script_string.0.as_str(),
+                args = s
+            )))
+            .expect("Can't execute script")
     };
 
-    if result.is_err() {
-        if args.get("url").is_none() {
-            return url.to_string();
-        } else {
-            let args_url = args.get("url").and_then(|x| x.as_str()).unwrap_or("");
-            return args_url.to_string();
-        }
-    }
+    let convert_result_to_rust_string = result
+        .as_string()
+        .expect("Can't convert to string")
+        .to_std_string()
+        .expect("Can't convert to string");
 
-    let is_result_string = result.as_ref().unwrap().as_string();
+    let mut return_url = url::Url::parse(&url).expect("Can't parse the url");
 
-    if is_result_string.is_none() {
-        if args.get("url").is_none() {
-            return url.to_string();
-        } else {
-            let args_url = args.get("url").and_then(|x| x.as_str()).unwrap_or("");
-            return args_url.to_string();
-        }
-    }
-
-    let convert_result_to_rust_string = is_result_string.unwrap().to_std_string();
-
-    if convert_result_to_rust_string.is_err() {
-        if args.get("url").is_none() {
-            return url.to_string();
-        } else {
-            let args_url = args.get("url").and_then(|x| x.as_str()).unwrap_or("");
-            return args_url.to_string();
-        }
-    }
-
-    let result = convert_result_to_rust_string.unwrap();
-
-    let return_url = url::Url::parse(args.get("url").and_then(|x| x.as_str()).unwrap_or(""));
-
-    if return_url.is_err() {
-        if args.get("url").is_none() {
-            return url.to_string();
-        } else {
-            let args_url = args.get("url").and_then(|x| x.as_str()).unwrap_or("");
-            return args_url.to_string();
-        }
-    }
-
-    let mut return_url = return_url.unwrap();
-
-    let query_name = if args.get("sp").is_some() {
-        args.get("sp")
-            .and_then(|x| x.as_str())
-            .unwrap_or("signature")
-    } else {
-        "signature"
-    };
+    let query_name = args.sp.unwrap_or_else(|| "signature".to_owned());
 
     let mut query = return_url
         .query_pairs()
         .map(|(name, value)| {
             if name == query_name {
-                (name.into_owned(), result.to_string())
+                (name.into_owned(), convert_result_to_rust_string.to_string())
             } else {
                 (name.into_owned(), value.into_owned())
             }
@@ -119,7 +82,7 @@ pub fn decipher(
         .collect::<Vec<(String, String)>>();
 
     if !return_url.query_pairs().any(|(name, _)| name == query_name) {
-        query.push((query_name.to_string(), result));
+        query.push((query_name.to_string(), convert_result_to_rust_string));
     }
 
     return_url.query_pairs_mut().clear().extend_pairs(&query);
